1960b9318443e01b568de59417fdcf255dc85d0b
dhaifan999 <96604685+dhaifan999@users.noreply.github.com>
2022-12-27 23:37:16 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,6 +91,27 @@
     onscroll(document, headerScrolled)
   }
 
+   /**
+   * getting modal opening buttons
+   */
+  var modalBtns = document.querySelectorAll(".modal-open");
+
+  modalBtns.forEach(function(btn) {
+    btn.onclick = function() {
+      var modal = btn.getAttribute("data-modal");
+
+      document.getElementById(modal).style.display = "block";
+    };
+  });
+
+  var closeBtns = document.querySelectorAll('.modal-close');
+
+  closeBtns.forEach(function(btn) {
+    btn.onclick = function() {
+      var modal = (btn.closest('.modal').style.display = "none");
+    };
+  });
+
   /**
    * Back to top button
    */
